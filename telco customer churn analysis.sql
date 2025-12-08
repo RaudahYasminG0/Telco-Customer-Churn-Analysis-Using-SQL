89b91a4486f322c5ffe5267a3dfc7d40759cf235
Raudah Yasmin Ghozali <raudahyasminghozali@gmail.com>
2025-12-08 16:14:28 +0700

    Update telco customer churn analysis.sql

--- telco customer churn analysis.sql
+++ telco customer churn analysis.sql
@@ -10,13 +10,13 @@ alter table `WA_Fn-UseC_-Telco-Customer-Churn` rename to telco_cust_churn;
 -- Menampilkan seluruh data dalam table
 select*from telco_cust_churn;
 
-
 							-- TELCO CUST CHURN ANALYSIS --
 
 -- Statistika deskriptif--
+
 (select 'Count' as ` `,count(tenure) as `Tenure`, count(MonthlyCharges) as `Monthly Charges`, count(TotalCharges) as `Total Charges` from telco_cust_churn)
 union
-(select 'Means' ,round(avg(tenure),2), round(avg(MonthlyCharges),2), round(avg(TotalCharges),2) from telco_cust_churn)
+(select 'Mean' ,round(avg(tenure),2), round(avg(MonthlyCharges),2), round(avg(TotalCharges),2) from telco_cust_churn)
 union
 (select 'Min', round(min(tenure),2), round(min(MonthlyCharges),2), round(min(TotalCharges),2) from telco_cust_churn)
 union
@@ -24,58 +24,90 @@ union
 
 -- Churn rate by gender--
 -- Not churn
-select gender as Gender, count(case when Churn = 'No' then customerID end) as ` Not churn rate`, concat(round(count(customerID)*100/(select count(customerID) from telco_cust_churn),2),'%') as ` Not churn rate (%)` 
+select gender as Gender, count(case when Churn = 'No' then customerID end) as `Not churn rate`, concat(round(count(customerID)*100/(select count(customerID) from telco_cust_churn),2),'%') as ` Not churn rate (%)` 
 from telco_cust_churn group by gender;
 -- Churn
-select gender as Gender, count(case when Churn = 'Yes' then customerID end) as ` Churn rate`, concat(round(count(customerID)*100/(select count(customerID) from telco_cust_churn),2),'%') as ` Churn rate (%)` 
+select gender as Gender, count(case when Churn = 'Yes' then customerID end) as `Churn rate`, concat(round(count(customerID)*100/(select count(customerID) from telco_cust_churn),2),'%') as ` Churn rate (%)` 
 from telco_cust_churn group by gender;
+
+-- Churn rate by Senior Citizen--
+-- Not churn
+select SeniorCitizen as `Senior Citizen`, count(case when Churn = 'No' then customerID end) as `Not churn rate`, concat(round(count(customerID)*100/(select count(customerID) from telco_cust_churn),2),'%') as ` Not churn rate (%)` 
+from telco_cust_churn group by SeniorCitizen;
+-- Churn
+select SeniorCitizen as `Senior Citizen`, count(case when Churn = 'Yes' then customerID end) as `Churn rate`, concat(round(count(customerID)*100/(select count(customerID) from telco_cust_churn),2),'%') as ` Churn rate (%)` 
+from telco_cust_churn group by SeniorCitizen;
 
 -- Churn rate by partner--
 -- Not churn
-select Partner, count(case when Churn = 'No' then customerID end) as ` Not churn rate`, concat(round(count(customerID)*100/(select count(customerID) from telco_cust_churn),2),'%') as ` Not churn rate (%)` 
+select Partner, count(case when Churn = 'No' then customerID end) as `Not churn rate`, concat(round(count(customerID)*100/(select count(customerID) from telco_cust_churn),2),'%') as `Not churn rate (%)` 
 from telco_cust_churn group by partner;
 -- Churn
-select Partner, count(case when Churn = 'Yes' then customerID end) as ` Churn rate`, concat(round(count(customerID)*100/(select count(customerID) from telco_cust_churn),2),'%') as ` Churn rate (%)` 
+select Partner, count(case when Churn = 'Yes' then customerID end) as `Churn rate`, concat(round(count(customerID)*100/(select count(customerID) from telco_cust_churn),2),'%') as `Churn rate (%)` 
 from telco_cust_churn group by partner;
+
+-- Churn rate by dependents--
+-- Not churn
+select Dependents, count(case when Churn = 'No' then customerID end) as `Not churn rate`, concat(round(count(customerID)*100/(select count(customerID) from telco_cust_churn),2),'%') as `Not churn rate (%)` 
+from telco_cust_churn group by Dependents;
+-- Churn
+select Dependents, count(case when Churn = 'Yes' then customerID end) as `Churn rate`, concat(round(count(customerID)*100/(select count(customerID) from telco_cust_churn),2),'%') as `Churn rate (%)` 
+from telco_cust_churn group by Dependents;
 
 -- Churn rate by internet service--
 -- Not churn
-select InternetService as `Internet Service`, count(case when Churn = 'No' then customerID end) as ` Not churn rate`, concat(round(count(customerID)*100/(select count(customerID) from telco_cust_churn),2),'%') as ` Not churn rate (%)` 
+select InternetService as `Internet Service`, count(case when Churn = 'No' then customerID end) as `Not churn rate`, concat(round(count(customerID)*100/(select count(customerID) from telco_cust_churn),2),'%') as `Not churn rate (%)` 
 from telco_cust_churn group by InternetService;
 -- Churn
-select InternetService as `Internet Service`, count(case when Churn = 'Yes' then customerID end) as ` Not churn rate`, concat(round(count(customerID)*100/(select count(customerID) from telco_cust_churn),2),'%') as ` Not churn rate (%)` 
+select InternetService as `Internet Service`, count(case when Churn = 'Yes' then customerID end) as `Churn rate`, concat(round(count(customerID)*100/(select count(customerID) from telco_cust_churn),2),'%') as `Churn rate (%)` 
 from telco_cust_churn group by InternetService;
+
+-- Churn rate by online security--
+-- Not churn
+select OnlineSecurity as `Online Security`, count(case when Churn = 'No' then customerID end) as `Not churn rate`, concat(round(count(customerID)*100/(select count(customerID) from telco_cust_churn),2),'%') as `Not churn rate (%)` 
+from telco_cust_churn group by OnlineSecurity;
+-- Churn
+select OnlineSecurity as `Online Security`, count(case when Churn = 'Yes' then customerID end) as `Churn rate`, concat(round(count(customerID)*100/(select count(customerID) from telco_cust_churn),2),'%') as `Churn rate (%)`
+from telco_cust_churn group by OnlineSecurity;
+
+-- Churn rate by device protection--
+-- Not churn
+select DeviceProtection as `Device Protection`, count(case when Churn = 'No' then customerID end) as `Not churn rate`, concat(round(count(customerID)*100/(select count(customerID) from telco_cust_churn),2),'%') as `Not churn rate (%)` 
+from telco_cust_churn group by DeviceProtection;
+-- Churn
+select DeviceProtection as `Device Protection`, count(case when Churn = 'Yes' then customerID end) as `Churn rate`, concat(round(count(customerID)*100/(select count(customerID) from telco_cust_churn),2),'%') as `Churn rate (%)`
+from telco_cust_churn group by DeviceProtection;
 
 -- Churn rate by streaming movies--
 -- Not churn
-select StreamingMovies as `Streaming Movies`, count(case when Churn = 'No' then customerID end) as ` Not churn rate`, concat(round(count(customerID)*100/(select count(customerID) from telco_cust_churn),2),'%') as ` Not churn rate (%)` 
+select StreamingMovies as `Streaming Movies`, count(case when Churn = 'No' then customerID end) as `Not churn rate`, concat(round(count(customerID)*100/(select count(customerID) from telco_cust_churn),2),'%') as `Not churn rate (%)` 
 from telco_cust_churn group by StreamingMovies;
 -- Churn
-select StreamingMovies as `Streaming Movies`, count(case when Churn = 'Yes' then customerID end) as ` Not churn rate`, concat(round(count(customerID)*100/(select count(customerID) from telco_cust_churn),2),'%') as ` Not churn rate (%)` 
+select StreamingMovies as `Streaming Movies`, count(case when Churn = 'Yes' then customerID end) as `Churn rate`, concat(round(count(customerID)*100/(select count(customerID) from telco_cust_churn),2),'%') as `Churn rate (%)` 
 from telco_cust_churn group by StreamingMovies;
 
 -- Churn rate by streaming tv--
 -- Not churn
-select StreamingTV as `Streaming TV`, count(case when Churn = 'No' then customerID end) as ` Not churn rate`, concat(round(count(customerID)*100/(select count(customerID) from telco_cust_churn),2),'%') as ` Not churn rate (%)` 
+select StreamingTV as `Streaming TV`, count(case when Churn = 'No' then customerID end) as `Not churn rate`, concat(round(count(customerID)*100/(select count(customerID) from telco_cust_churn),2),'%') as `Not churn rate (%)` 
 from telco_cust_churn group by StreamingTV;
 -- Churn
-select StreamingTV as `Streaming TV`, count(case when Churn = 'Yes' then customerID end) as ` Not churn rate`, concat(round(count(customerID)*100/(select count(customerID) from telco_cust_churn),2),'%') as ` Not churn rate (%)` 
+select StreamingTV as `Streaming TV`, count(case when Churn = 'Yes' then customerID end) as `Churn rate`, concat(round(count(customerID)*100/(select count(customerID) from telco_cust_churn),2),'%') as `Churn rate (%)`
 from telco_cust_churn group by StreamingTV;
 
 -- Churn rate by contract--
 -- Not churn
-select Contract, count(case when Churn = 'No' then customerID end) as ` Not churn rate`, concat(round(count(customerID)*100/(select count(customerID) from telco_cust_churn),2),'%') as ` Not churn rate (%)` 
+select Contract, count(case when Churn = 'No' then customerID end) as `Not churn rate`, concat(round(count(customerID)*100/(select count(customerID) from telco_cust_churn),2),'%') as `Not churn rate (%)` 
 from telco_cust_churn group by Contract;
 -- Churn
-select Contract, count(case when Churn = 'Yes' then customerID end) as ` Not churn rate`, concat(round(count(customerID)*100/(select count(customerID) from telco_cust_churn),2),'%') as ` Not churn rate (%)` 
+select Contract, count(case when Churn = 'Yes' then customerID end) as `Churn rate`, concat(round(count(customerID)*100/(select count(customerID) from telco_cust_churn),2),'%') as `Churn rate (%)` 
 from telco_cust_churn group by Contract;
 
 -- Churn rate by payment method--
 -- Not churn
-select PaymentMethod as `Payment Method`, count(case when Churn = 'No' then customerID end) as ` Not churn rate`, concat(round(count(customerID)*100/(select count(customerID) from telco_cust_churn),2),'%') as ` Not churn rate (%)` 
+select PaymentMethod as `Payment Method`, count(case when Churn = 'No' then customerID end) as `Not churn rate`, concat(round(count(customerID)*100/(select count(customerID) from telco_cust_churn),2),'%') as `Not churn rate (%)` 
 from telco_cust_churn group by PaymentMethod;
 -- Churn
-select PaymentMethod as `Payment Method`, count(case when Churn = 'Yes' then customerID end) as ` Not churn rate`, concat(round(count(customerID)*100/(select count(customerID) from telco_cust_churn),2),'%') as ` Not churn rate (%)` 
+select PaymentMethod as `Payment Method`, count(case when Churn = 'Yes' then customerID end) as `Churn rate`, concat(round(count(customerID)*100/(select count(customerID) from telco_cust_churn),2),'%') as `Churn rate (%)`
 from telco_cust_churn group by PaymentMethod;
 
 -- Total customer by churn rate
